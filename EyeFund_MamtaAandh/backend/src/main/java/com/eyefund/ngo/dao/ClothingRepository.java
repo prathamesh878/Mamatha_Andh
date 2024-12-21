e8cab339d94ca15a279cb639f90a7f831eb9c6a4
Aditya <adimail2404@gmail.com>
2024-12-21 10:12:09 +0530

    Folder structure update for github deployment

--- EyeFund_MamtaAandh/backend/src/main/java/com/eyefund/ngo/dao/ClothingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eyefund.ngo.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.eyefund.ngo.model.Cloth;
-
-public interface ClothingRepository extends MongoRepository<Cloth, String> {
-
-}
